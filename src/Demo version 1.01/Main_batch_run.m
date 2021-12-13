1ea7f7f3c88b1d001e4f251ea66b0f2ab7a90c8b
Rayinfluense <samuel125807@hotmail.com>
2021-12-13 16:02:58 +0100

    Updated batch run

--- src/Demo version 1.01/Main_batch_run.m
+++ src/Demo version 1.01/Main_batch_run.m
@@ -1,31 +1,32 @@
 clear all
 clc
-avgTravelTimeMultRuns = zeros(1,3); %An avg for each awarenessType.
-seedVec = 1:3;
-citySizeVec = 5:2:9;
+seedVec = 1:2;
+citySizeVec = [7,13,15];
+avgTravelTimeMultRuns = zeros(length(citySizeVec),3); %An avg for each awarenessType.
 progress = 0;
 for seed = seedVec
-    for cityLength = citySizeVec
+    for citySizeCount = 1:length(citySizeVec)
+        cityLength = citySizeVec(citySizeCount);
         progress = progress + 1;
         disp("Progress: " + num2str(progress/(length(seedVec)*length(citySizeVec))))
         parfor awarenessType = 0:2
-            close all
+            %close all
             rng('default');
             rng(seed);
             %Parameters to edit:
             %---------------------------------------
             %Graphic detail = 3 or 15 is recommended.
             graphicDetail = -1; %-1 is no graphics, 0 Is simple mode, 1 is advanced printout, 2 + is advanced printout with interpolation for graphicDetail interpolation steps.
-            set(gcf, 'Position', [50,50,1600,900])
+            %set(gcf, 'Position', [50,50,1600,900])
             %citySize = 17; %MUST BE ODD APPARENTLY
             nIndividuals = round((cityLength^2)*(4+1*rand));
             spawnFunction = @(t) (cityLength)*(rand/4+1.5)*exp(-t/25);
             %awarenessType = 0; %I'm guessing this will be set once for the entire simulation.
             %---------------------------------------
 
-            v = VideoWriter("awareness_" + num2str(awarenessType),'MPEG-4');
-            v.FrameRate = max(4,2*graphicDetail);
-            open(v);
+            v = NaN;
+            %v.FrameRate = max(4,2*graphicDetail);
+            %open(v);
             [adjMat,capacityMat] = RS_RoadGen(cityLength);
             citySize = length(adjMat);
             G = graph(adjMat(:,:,1));
@@ -41,21 +42,27 @@ for seed = seedVec
             for i = 1:nIndividuals
                 individuals{i} = struct('route',routes(i),'routeStep',1,'roadProgress',1,'totTravelTime',0,'identifier',1,'queueTime',0,'haveDibs',0);
             end
+            disp("For citySize: " + num2str(cityLength))
             avgTravelTime = RunSim(individuals,A,v,graphicDetail,awarenessType,nodeList,globalEventQueue,citySize,liveMap,spawnFunction);
             title("Average travel time was: " + num2str(avgTravelTime) + " units.")
-            drawnow
-            frame = getframe(gcf);
-            for i = 1:2*v.FrameRate
-                writeVideo(v,frame)
-            end
-            close(v);
-            avgTravelTimeMultRuns(awarenessType+1) = avgTravelTimeMultRuns(awarenessType+1) + avgTravelTime;
+            %drawnow
+            %frame = getframe(gcf);
+            %for i = 1:2*v.FrameRate
+            %    writeVideo(v,frame)
+            %end
+            %close(v);
+            avgTravelTimeMultRuns(citySizeCount,awarenessType+1) = avgTravelTimeMultRuns(citySizeCount,awarenessType+1) + avgTravelTime;
         end
     end
 end
 
-avgTravelTimeMultRuns = avgTravelTimeMultRuns / (length(seedVec)*length(citySizeVec));
-bar(avgTravelTimeMultRuns)
-xlabel('Awareness Type')
-ylabel("Average Travel Time")
-axis([0 4 50 100])
+avgTravelTimeMultRuns = avgTravelTimeMultRuns / length(seedVec);
+
+for i = 1:length(citySizeVec)
+    subplot(1,length(citySizeVec),i)
+    bar(avgTravelTimeMultRuns(i,:))
+    xlabel('Awareness Type')
+    ylabel("Average Travel Time")
+    title("City size: " + num2str(citySizeVec(i)))
+    axis([0 4 50 100])
+end
