d37e5ddd7257e57792d47972a28f45162ea4ab42
Rayinfluense <samuel125807@hotmail.com>
2021-12-29 19:10:27 +0100

    Added errorbars

--- src/Demo version 1.01/Main_batch_run_FASTAF.m
+++ src/Demo version 1.01/Main_batch_run_FASTAF.m
@@ -1,14 +1,15 @@
 clear all
 clc
-seedVec = 1:10;
-citySizeVec = [7,13,17];
+seedVec = 1:2; %MUST GO FROM 1 AND UP
+citySizeVec = [5];
 avgTravelTimeMultRuns = zeros(1,length(citySizeVec)*3); %An avg for each awarenessType.
+fullLog = zeros(length(seedVec),length(citySizeVec)*3);
 seedLength = length(seedVec);
 normalizeMat = ones(1,length(citySizeVec)*3)*seedLength;
 progress = 0;
 for seed = seedVec        
     progress = progress + 1;
-    disp("Progress: " + num2str(progress/(length(seedVec)*length(citySizeVec))))
+    disp("Progress: " + num2str(progress/(length(seedVec))))
     parfor i = 1:length(citySizeVec)*3
         citySizeCount = ceil(i/3);
         awarenessType = mod(i-1,3);
@@ -21,7 +22,7 @@ for seed = seedVec
         %Graphic detail = 3 or 15 is recommended.
         graphicDetail = -1; %-1 is no graphics, 0 Is simple mode, 1 is advanced printout, 2 + is advanced printout with interpolation for graphicDetail interpolation steps.
         %set(gcf, 'Position', [50,50,1600,900])
-        %citySize = 17; %MUST BE ODD APPARENTLY
+        %citySize = 17; %M3UST BE ODD APPARENTLY
         nIndividuals = round((cityLength^2)*(3.8+1*rand));
         spawnFunction = @(t) (cityLength)*(rand/4+1.4)*exp(-t/20);
         %awarenessType = 0; %I'm guessing this will be set once for the entire simulation.
@@ -55,6 +56,7 @@ for seed = seedVec
         %end
         %close(v);
         avgTravelTimeMultRuns(i) = avgTravelTimeMultRuns(i) + avgTravelTime;
+        fullLog(seed,i) = avgTravelTime;
         if avgTravelTime == 0
             normalizeMat(i) = normalizeMat(i) - 1;
         end
@@ -62,19 +64,28 @@ for seed = seedVec
 end
 
 avgTravelTimeMultRuns = avgTravelTimeMultRuns ./ normalizeMat;
+vars = zeros(1,length(citySizeVec)*3);
+for i = 1:length(vars)
+    log = fullLog(:,i);
+    vars(i) = var(log(log~=0));
+end
 
 avgTravelTimeMultRunsMat = zeros(length(citySizeVec),3);
+varsMat = avgTravelTimeMultRunsMat;
 for i = 1:length(avgTravelTimeMultRuns)
     citySizeCount = ceil(i/3);
     awarenessType = mod(i-1,3);
     avgTravelTimeMultRunsMat(citySizeCount,awarenessType+1) = avgTravelTimeMultRuns(i);
+    varsMat(citySizeCount,awarenessType+1) = vars(i);
 end
 
 for i = 1:length(citySizeVec)
     subplot(1,length(citySizeVec),i)
     bar(avgTravelTimeMultRunsMat(i,:))
+    hold on
     xlabel('Awareness Type')
     ylabel("Average Travel Time")
     title("City size: " + num2str(citySizeVec(i)))
     axis([0 4 40 150])
+    errorbar([1,2,3],avgTravelTimeMultRunsMat(i,:),-sqrt(varsMat(i,:)),sqrt(varsMat(i,:)),'LineStyle','none');
 end
